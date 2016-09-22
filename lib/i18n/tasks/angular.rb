a10c5e83868643667caf26f691ea03e82ce15ee6
Tomasz Pajor <tomek@polishgeeks.com>
2016-09-22 13:57:16 +0200

    add support for html files

--- lib/i18n/tasks/angular.rb
+++ lib/i18n/tasks/angular.rb
@@ -5,22 +5,22 @@ require 'i18n/tasks/angular/version'
 require 'i18n/tasks/angular/scanners/translate_abstract'
 
 require 'i18n/tasks/angular/scanners/translate'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::Translate', only: %w(*.haml *.coffee *.js)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::Translate', only: %w(*.html *.haml *.coffee *.js)
 
 require 'i18n/tasks/angular/scanners/translate_once'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslateOnce', only: %w(*.haml *.coffee *.js)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslateOnce', only: %w(*.html *.haml *.coffee *.js)
 
 require 'i18n/tasks/angular/scanners/translate_instant'
 I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslateInstant', only: %w(*.coffee *.js)
 
 require 'i18n/tasks/angular/scanners/translate_plural'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslatePlural', only: %w(*.haml)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslatePlural', only: %w(*.html *.haml)
 
 require 'i18n/tasks/angular/scanners/translate_pluralize'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslatePluralize', only: %w(*.haml)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslatePluralize', only: %w(*.html *.haml)
 
 require 'i18n/tasks/angular/scanners/translation_key'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslationKey', only: %w(*.haml *.coffee *.js)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslationKey', only: %w(*.html *.haml *.coffee *.js)
 
 require 'i18n/tasks/angular/scanners/translation_uib_tooltip'
-I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslationUibTooltip', only: %w(*.haml)
+I18n::Tasks.add_scanner 'I18n::Tasks::Angular::Scanners::TranslationUibTooltip', only: %w(*.html *.haml)
